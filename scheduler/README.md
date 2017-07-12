64250d8ac6c408be05ce0e552da555028d086fc8
Patrick Kleindienst <patrick.kleindienst@web.de>
2017-07-12 11:33:06 +0200

    Initial commit

---
+++ scheduler/README.md
@@ -0,0 +1,1 @@
+# aurora-scheduler-docker
---
+++ scheduler/scheduler.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AURORA_HOME=/usr/sbin
+
+AURORA_FLAGS=(
+ -http_port=${HTTP_PORT:-8080}
+ -cluster_name=${CLUSTER_NAME:-aurora}
+ -backup_dir=${BACKUP_DIR:-/tmp/aurora/backup}
+ -mesos_master_address=${MESOS_MASTERS:?"MESOS_MASTERS must not be empty"}
+ -serverset_path=${AURORA_ZK_PATH:-/aurora}
+ -zk_endpoints=${ZK_ENDPOINTS:?"ZK_ENDPOINTS must not be empty"}
+ -native_log_file_path=${NATIVE_LOG_FILE_PATH:-/var/lib/aurora/scheduler/db}
+ -native_log_zk_group_path=${NATIVE_LOG_ZK_GROUP_PATH:-/tmp/aurora/replicated-log}
+ -hostname=${HOSTNAME:?"HOSTNAME must not be empty"}
+ -native_log_quorum_size=${NATIVE_LOG_QUORUM_SIZE:-1}
+ -allowed_container_types=${ALLOWED_CONTAINER_TYPES:-MESOS,DOCKER}
+ -thermos_executor_path=${THERMOS_EXECUTOR_PATH:-/usr/share/aurora/bin/thermos_executor.pex}
+ -allow_docker_parameters=true
+)
+
+
+exec "$AURORA_HOME/aurora-scheduler" "${AURORA_FLAGS[@]}"
