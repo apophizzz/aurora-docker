33d0065653775cff4eb119bf2a95b7cf513e4ccf
Patrick.Kleindienst <patrick.kleindienst@web.de>
2017-09-24 14:05:27 +0200

    Correct checking against empty env values

--- executor/thermos.sh
+++ executor/thermos.sh
@@ -2,32 +2,32 @@
 
 # Check required Mesos env variables
 
-if [ -z ${MESOS_PORT} ]; then
+if [ -z "$MESOS_PORT" ]; then
 	echo "MESOS_PORT must not be empty"
 	exit 1
 fi
 
-if [ -z ${MESOS_MASTER} ]; then
+if [ -z "$MESOS_MASTER" ]; then
 	echo "MESOS_MASTER must not be empty"
 	exit 1
 fi
 
-if [ -z ${MESOS_SWITCH_USER} ]; then
+if [ -z "$MESOS_SWITCH_USER" ]; then
 	echo "MESOS_SWITCH_USER must not be empty"
 	exit 1
 fi 
 
-if [ -z ${MESOS_CONTAINERIZERS} ]; then
+if [ -z "$MESOS_CONTAINERIZERS" ]; then
 	echo "MESOS_CONTAINERIZERS must not be empty"
 	exit 1
 fi
 
-if [ -z ${MESOS_LOG_DIR} ]; then
+if [ -z "$MESOS_LOG_DIR" ]; then
 	echo "MESOS_LOG_DIR must not be empty"
 	exit 1
 fi
 
-if [ -z ${MESOS_WORK_DIR} ]; then
+if [ -z "$MESOS_WORK_DIR" ]; then
 	echo "MESOS_WORK_DIR must not be empty"
 	exit 1
 fi
@@ -35,7 +35,7 @@ fi
 
 # Check required Thermos executor env variables
 
-if [ -z ${MESOS_ROOT} ]; then
+if [ -z "$MESOS_ROOT" ]; then
 	echo "MESOS_ROOT is not set, will be set to MESOS_WORK_DIR:$MESOS_WORK_DIR"
 	MESOS_ROOT=$MESOS_WORK_DIR
 elif [ "$MESOS_ROOT" != "$MESOS_WORK_DIR" ]; then
