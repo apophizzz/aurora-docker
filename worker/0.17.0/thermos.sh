90c4f3451862353844a7d47d8e519f6aceb5a223
Patrick.Kleindienst <patrick.kleindienst@web.de>
2017-10-23 11:53:33 +0200

    Observer and agent process now controlled by user 'aurora', fixes #4

--- worker/0.17.0/thermos.sh
+++ worker/0.17.0/thermos.sh
@@ -15,7 +15,7 @@ fi
 if [ -z "$MESOS_SWITCH_USER" ]; then
 	echo "MESOS_SWITCH_USER must not be empty"
 	exit 1
-fi 
+fi
 
 if [ -z "$MESOS_CONTAINERIZERS" ]; then
 	echo "MESOS_CONTAINERIZERS must not be empty"
@@ -41,19 +41,19 @@ if [ -z "$MESOS_ROOT" ]; then
 elif [ "$MESOS_ROOT" != "$MESOS_WORK_DIR" ]; then
 	echo "MESOS_ROOT is set to $MESOS_ROOT, but must match MESOS_WORK_DIR:$MESOS_WORK_DIR"
 	exit 1
-         
+
 fi
 
+chown -R $USER:$USER $MESOS_ROOT $MESOS_WORK_DIR $MESOS_LOG_DIR
+
 echo "Starting thermos ..."
-nohup /usr/sbin/thermos_observer --port=${HTTP_PORT:-1338} \
+sudo -E -u aurora nohup /usr/sbin/thermos_observer --port=${HTTP_PORT:-1338} \
  --mesos-root=${MESOS_ROOT} \
  --app_daemonize \
  --log_to_disk=NONE \
- --log_to_stderr=google:INFO 
+ --log_to_stderr=google:INFO
 
 echo "Starting Mesos slave ..."
-nohup mesos-slave 
+sudo -E -u aurora nohup mesos-slave
 
 wait
-
-
